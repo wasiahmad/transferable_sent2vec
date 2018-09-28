27e20c1aa0b29e2701c05e57c9be06195c208e40
wasiahmad <wasiahmad@ucla.edu>
2018-09-28 16:40:43 -0700

    adding bash files

---
+++ mtl_sent2vec/fshared.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+CUDA_VISIBLE_DEVICES=$1 python -W ignore fully_shared/main.py \
+    --cuda \
+    --task snli quora multinli \
+    --data ../data/ \
+    --max_example -1 \
+    --classifier 0 \
+    --model LSTM \
+    --bidirection \
+    --optimizer sgd,lr=0.1 \
+    --lrshrink 5 \
+    --minlr 1e-5 \
+    --emsize 300 \
+    --nhid 2048 \
+    --nlayers 1 \
+    --fc_dim 512 \
+    --lr_decay 0.99 \
+    --max_norm 5.0 \
+    --batch_size 64 \
+    --epochs 50 \
+    --dropout 0.2 \
+    --dropout_fc 0.2 \
+    --print_every 20 \
+    --save_path ./iclr_output/3tasks/ \
+    --word_vectors_directory ../data/glove/ \
+    --word_vectors_file glove.840B.300d.txt \
